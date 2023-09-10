909b551eec6f73adccb34aed18bab61f8a27e441
miliereya <105806526+miliereya@users.noreply.github.com>
2023-09-10 13:02:17 -0400

    Update useMoveAudio.ts

--- src/game/hooks/useMoveAudio.ts
+++ src/game/hooks/useMoveAudio.ts
@@ -1,18 +1,21 @@
 'use client'
 import { useEffect, useMemo, useState } from 'react'
 
-
-const playerMoveSound = typeof Audio !== undefined && new Audio(`/player_move.ogg`)
-
 export const useMoveAudio = (
 	isPlayerMoving: boolean | undefined
 	// isEnemyMoving: boolean
 ) => {
 	const [prev, setPrev] = useState(false)
+	const playerMoveSound =
+		typeof Audio !== undefined && new Audio(`/player_move.ogg`)
 	// const [enemyMove, setEnemyMove] = useState(false)
 
 	useEffect(() => {
-		if (isPlayerMoving !== undefined && isPlayerMoving !== prev && playerMoveSound) {
+		if (
+			isPlayerMoving !== undefined &&
+			isPlayerMoving !== prev &&
+			playerMoveSound
+		) {
 			isPlayerMoving ? playerMoveSound.play() : playerMoveSound.pause()
 			setPrev(isPlayerMoving)
 		}
