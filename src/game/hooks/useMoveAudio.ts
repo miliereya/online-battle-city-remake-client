d2c79cd2defcfe259bcf5c1ed79be5f801054bf1
miliereya <105806526+miliereya@users.noreply.github.com>
2023-09-10 13:13:51 -0400

    Update useMoveAudio.ts

--- src/game/hooks/useMoveAudio.ts
+++ src/game/hooks/useMoveAudio.ts
@@ -1,22 +1,25 @@
 'use client'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 export const useMoveAudio = (
 	isPlayerMoving: boolean | undefined
 	// isEnemyMoving: boolean
 ) => {
 	const [prev, setPrev] = useState(false)
-	const playerMoveSound =
-		typeof Audio !== undefined && new Audio(`/player_move.ogg`)
-	// const [enemyMove, setEnemyMove] = useState(false)
+	const playerMoveSound = useRef<HTMLAudioElement | null>(null)
 
+	useEffect(() => {
+		playerMoveSound.current = new Audio(`/player_move.ogg`)
+	}, [])
 	useEffect(() => {
 		if (
 			isPlayerMoving !== undefined &&
 			isPlayerMoving !== prev &&
-			playerMoveSound
+			playerMoveSound.current
 		) {
-			isPlayerMoving ? playerMoveSound.play() : playerMoveSound.pause()
+			isPlayerMoving
+				? playerMoveSound.current.play()
+				: playerMoveSound.current.pause()
 			setPrev(isPlayerMoving)
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
